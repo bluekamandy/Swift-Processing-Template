bdff6650393319f4889dbafd044890b57b70d166
Masood Kamandy <masood@masoodkamandy.com>
2021-06-15 08:34:22 -0700

    Cleaned up and ready to branch from here.

--- SwiftProcessingTemplate/Sketch.swift
+++ SwiftProcessingTemplate/Sketch.swift
@@ -1,14 +1,15 @@
+/*
+ * Basic Swift Processing Template
+ */
+
 import SwiftProcessing
 import UIKit
 
 class MySketch: Sketch, SketchDelegate {
     func setup() {
-        background(255, 0, 0)
-        stroke(255,255,255)
     }
     
     func draw() {
-        line(random() * width, random() * height, random() * width, random() * height)
     }
 }
 
